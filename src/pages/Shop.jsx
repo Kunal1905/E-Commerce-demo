a6eb6270fef08cc799fb719fc25f882b67ba93e9
Kunal1905 <kunalsuthar@Kunals-MacBook-Air.local>
2025-08-24 11:01:49 +0530

    my changes

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,7 +2,7 @@ import Popular from '../components/Popular/Popular'
 import Hero from '../components/Hero/Hero'
 import React from 'react'
 import Offers from '../components/Offers/Offers'
-import NewCollections from '../components/NewCollections/NewCOllections'
+import NewCollections from '../components/NewCollections/NewCollections'
 import NewsLetter from '../components/NewsLetter/NewsLetter'
 
 const Shop = () => {
